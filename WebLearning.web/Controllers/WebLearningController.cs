753f2ec27ff6c7d6c39ff13a6488393711b6e211
DESKTOP-7IJCCPL\Fonsei <manuel.karl88@gmail.com>
2018-01-29 08:15:28 +0100

    Add project files.

---
+++ WebLearning.web/Controllers/WebLearningController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebLearning.web.Controllers
+{
+    public class WebLearningController : Controller
+    {
+        // GET: WebLearning
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Login(Login model)
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Register(Register model)
+        //{
+        //    return View();
+        //}
+    }
+}
